e40d6037f4ec5126ab1ebd23f35f60c9f645eaa6
washonrails <iamx.3ur@protonmail.com>
2023-02-07 22:20:13 -0300

    refactor: functions re-usable and change variables names..

--- js/app.js
+++ js/app.js
@@ -1,12 +1,31 @@
 const cityForm = document.querySelector('[data-js="change-location"]')
+const cityNameContainer = document.querySelector('[data-js="city-name"]')
+const cityWeatherContainer = document.querySelector('[data-js="city-weather"]')
+const cityTemperatureContainer = document.querySelector('[data-js="city-temperature"]')
+const cityCard = document.querySelector('[data-js="city-card"]')
+let timeImg = document.querySelector('[data-js="time"]')
+const timeIconContainer = document.querySelector('[data-js="time-icon"]')
 
 cityForm.addEventListener('submit', async event => {
 	event.preventDefault()
 	
 	const inputValue = event.target.city.value
 	const [{ Key, LocalizedName }] = await getCityData(inputValue)
-	const [{ WeatherText, Temperature }] = await getCityWeather(Key)
+	const [{ WeatherText, Temperature, IsDayTime, WeatherIcon }] = await getCityWeather(Key)
+	const timeIcon = `<img src="./src/icons/${WeatherIcon}.svg"/>`
 	
-	console.log(WeatherText, Temperature.Metric.Value)
+	cityCard.classList.contains('d-none') ? cityCard.classList.remove('d-none') : null
+	
+	if (IsDayTime) {
+		timeImg.src = '../src/day.svg'
+	}else {
+		timeImg.src = '../src/night.svg'
+	}
+	
+	timeIconContainer.innerHTML = timeIcon
+	cityNameContainer.textContent = LocalizedName
+	cityWeatherContainer.textContent = WeatherText
+	cityTemperatureContainer.textContent = Temperature.Metric.Value
+
 	cityForm.reset()
 })
